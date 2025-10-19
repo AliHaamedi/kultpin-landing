47a6f7f15f7199184bca423ed54c9226b9ea488e
Ali Haamedi <alihaamedi@gmail.com>
2025-10-19 14:31:45 +0330

    add footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,44 @@
+import Image from "next/image";
+
 const Footer = () => {
-  return <div className="py-96"></div>;
+  return (
+    <div className="py-32 flex flex-col items-center">
+      <Image
+        src="/icons/logo.svg"
+        alt="logo"
+        width={60}
+        height={60}
+        className="size-20"
+      />
+
+      <div className="flex items-center justify-center mt-5">
+        <h3 className="font-bold text-base">Ping Us Anytime | </h3>
+        <a
+          href="https://x.com/KultPin"
+          target="_blank"
+          className="cursor-pointer pl-2"
+        >
+          <Image
+            src="/icons/twitter.svg"
+            alt="logo"
+            width={20}
+            height={20}
+            className="size-5"
+          />
+        </a>
+        <a href="mailto:kultpinart@gmail.com" className="cursor-pointer pl-2">
+          <Image
+            src="/icons/mail.svg"
+            alt="logo"
+            width={20}
+            height={20}
+            className="size-5"
+          />
+        </a>
+      </div>
+      <p className="font-bold text-xs">© 2025 KultPin. All rights reserved</p>
+    </div>
+  );
 };
 
 export default Footer;
